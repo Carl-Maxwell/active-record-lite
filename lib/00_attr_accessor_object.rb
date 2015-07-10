3a5010790a7e67a50472aea5ff198a8237e21d08
Carl Maxwell <carl.maxwell@gmail.com>
2015-07-10 10:33:08 -0700

    adds ::my_attr_accessor & passes specs

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -1,5 +1,10 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      setter = "#{name}=".to_sym
+
+      define_method(name) { instance_variable_get("@#{name}") }
+      define_method(setter) { |value| instance_variable_set("@#{name}", value) }
+    end
   end
 end
